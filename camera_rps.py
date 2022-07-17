148e53922f5a2eb27fbc4da2b0cdbf727f2c5ad5
shrey0273 <71294496+shrey0273@users.noreply.github.com>
2022-07-17 20:01:00 +0100

    Fixes to give correct round number, and removes unnecessary extras

--- camera_rps.py
+++ camera_rps.py
@@ -4,17 +4,16 @@ from manual_rps import get_comp_choice,get_winner,options
 
 def new_prediction():
     distribution = list((activate_model())[0])
-    print("It is "+str(distribution))
-    input("Continue?")
     max_val = max(distribution)
     print(max_val)
     return distribution.index(max_val)
 
 def play():
-    games = 1
+    games = 0
     wins = 0
     losses = 0
     while wins!=3 and losses!=3:
+        games+=1
         input("You ready?")
         print("Okay let's go! Round "+str(games))
         comp_choice = get_comp_choice();
@@ -22,10 +21,8 @@ def play():
         print("The Computer Chose "+options[comp_choice]+" while you chose "+options[user_choice])
         result = get_winner(comp_choice,user_choice);
         if result=="Win!!":
-            games+=1
             wins+=1
         elif result=="Lose...":
-            games+=1
             losses+=1
         print("You "+result)
     # After the loop release the cap object
